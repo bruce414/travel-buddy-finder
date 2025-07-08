e47be010d3d520cb110788e6c68ec8d2fb367b4c
bruce414 <brucezhang414@gmail.com>
2025-07-08 12:10:06 +1200

    updated IUserRepository.cs, going to switch to main and add a new feature to repository so that users can get matchmakings, I will do this in a new branch called backend/update-repositories . ;

--- TravelBuddyApi/Repositories/Abstract/IUserRepository.cs
+++ TravelBuddyApi/Repositories/Abstract/IUserRepository.cs
@@ -6,6 +6,8 @@ public interface IUserRepository
 {
     Task<List<User>> GetAllUsersAsync();
     Task<User> GetUserByIdAsync(long id);
+    //Retrieve users with similar interests
+    Task<IEnumerable<User>> GetUsersBySimilarInterests();
     Task AddUserAsync(User user);
     Task UpdateUserAsync(User user);
     Task RemoveUserAsync(long id);
