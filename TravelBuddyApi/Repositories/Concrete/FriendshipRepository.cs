b407f1729fe8b01e50322d57de32abd3db8b113e
bruce414 <brucezhang414@gmail.com>
2025-07-09 15:16:42 +1200

    updated teh friendship repository, so that it can now retrieve users friends by userId

--- TravelBuddyApi/Repositories/Concrete/FriendshipRepository.cs
+++ TravelBuddyApi/Repositories/Concrete/FriendshipRepository.cs
@@ -6,6 +6,14 @@ namespace TravelBuddyApi.Repositories.Concrete;
 
 public class FriendshipRepository(TravelBuddyContext _travelBuddyContext)
 {
+    public async Task<List<Friendship>> GetFriendsByUserId(long userId)
+    {
+        return await _travelBuddyContext.Friendships
+                .Where(u => u.UserId == userId)
+                .Include(u => u.Friend)
+                .ToListAsync();
+    }
+    
     public async Task<List<Friendship>> GetFriendsByFrindshipStatusAsync(long userId, FriendshipStatus friendshipStatus)
     {
         return await _travelBuddyContext.Friendships
