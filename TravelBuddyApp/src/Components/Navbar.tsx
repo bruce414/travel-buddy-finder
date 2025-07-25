05cfb7b29566f0287afa770c4152337e532a2ff1
bruce414 <brucezhang414@gmail.com>
2025-07-25 12:12:49 +1200

    renamed the file and directory names

--- TravelBuddyApp/src/Components/Navbar.tsx
+++ TravelBuddyApp/src/Components/Navbar.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const handleNotifcation = (Event: React.MouseEvent<HTMLButtonElement>) => {
+    Event.preventDefault();
+    console.log("Notification button clicked");
+  }
+
   return (
-    <div>Navbar</div>
+    <>
+      <header>
+        <nav className='top-nav'>
+          <a href="/" className='logo'>TravelBuddyFinder</a>
+          <ul className='topnav-links'>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/trips'>Trips</a></li>
+            <li><a href='/connect'>Connect</a></li>
+          </ul>
+          <div className='admin-section'>
+            <button onClick={handleNotifcation} className='notification-button'>
+              <FontAwesomeIcon icon={faBell} size='lg' />
+            </button>
+          </div>
+        </nav>
+      </header>
+    </>
   )
 }
 
